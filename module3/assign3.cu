a03b08c01cdb7f3c77268c5ae34ad74f32a76fa4
wilpena01 <thewilsonflow@hotmail.com>
2022-02-11 15:54:51 -0500

    fix bug

--- module3/assign3.cu
+++ module3/assign3.cu
@@ -108,14 +108,14 @@ void main_sub0()
 	         int *gpu_subResult;
 	unsigned int *gpu_mulResult;
 	unsigned int *gpu_modResult;
-	unsigned *int gpu_addBlock[ARRAY_SIZE];
-	unsigned *int gpu_addThread[ARRAY_SIZE];	
-	unsigned *int gpu_subBlock[ARRAY_SIZE];
-	unsigned *int gpu_subThread[ARRAY_SIZE];	
-	unsigned *int gpu_mulBlock[ARRAY_SIZE];
-	unsigned *int gpu_mulThread[ARRAY_SIZE];	
-	unsigned *int gpu_modBlock[ARRAY_SIZE];
-	unsigned *int gpu_modThread[ARRAY_SIZE];	
+	unsigned *int gpu_addBlock;
+	unsigned *int gpu_addThread;	
+	unsigned *int gpu_subBlock;
+	unsigned *int gpu_subThread;	
+	unsigned *int gpu_mulBlock;
+	unsigned *int gpu_mulThread;	
+	unsigned *int gpu_modBlock;
+	unsigned *int gpu_modThread;	
 
 	cudaMalloc((void **)&gpu_arr1, ARRAY_SIZE_IN_BYTES);
 	cudaMalloc((void **)&gpu_arr2, ARRAY_SIZE_IN_BYTES);
