60535d682d517d68b929f03e047e24cce2e7c908
wilpena01 <thewilsonflow@hotmail.com>
2022-02-19 15:52:00 -0500

    organized

--- module4/assign4.cu
+++ module4/assign4.cu
@@ -133,7 +133,7 @@ int main(int argc, char** argv)
 
 
 	auto start1    = high_resolution_clock::now();	
-	main_Pinned(totalThreads, numBlocks, blockSize); 
+	main_Pegeable(totalThreads, numBlocks, blockSize); 
 	auto stop1     = high_resolution_clock::now();	
 	auto duration1 = duration_cast<microseconds>(stop1 - start1);
 
