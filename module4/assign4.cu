07a765fd46ef83465115c211f101374de08dcdb5
wilpena01 <thewilsonflow@hotmail.com>
2022-02-18 19:20:44 -0500

    fix bug

--- module4/assign4.cu
+++ module4/assign4.cu
@@ -23,11 +23,11 @@ struct RESULT
 void pushResult(unsigned int cpu_addResult, unsigned int cpu_addBlock, 
 unsigned int cpu_addThread, RESULT *finalResult, unsigned int ARRAY_SIZE)
 {
-	for(int i =0; i< ARRAY_SIZE; i++)
+	for(int i=0; i< ARRAY_SIZE; i++)
 	{
-		finalResult->result.push_back(cpu_addResult[i]);
-		finalResult->blockId.push_back(cpu_addBlock[i]);
-		finalResult->threadId.push_back(cpu_addThread[i]);
+		finalResult->result->push_back(cpu_addResult[i]);
+		finalResult->blockId->push_back(cpu_addBlock[i]);
+		finalResult->threadId->push_back(cpu_addThread[i]);
 	}
 
 }
