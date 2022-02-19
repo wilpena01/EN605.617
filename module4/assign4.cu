0665aea0dea595e78ec974cf93dcb961b82b5253
wilpena01 <thewilsonflow@hotmail.com>
2022-02-19 12:07:42 -0500

    organized

--- module4/assign4.cu
+++ module4/assign4.cu
@@ -90,7 +90,7 @@ void main_Pinned(unsigned int totalThreads, unsigned int numBlocks,
 	init(cpu_arr1, cpu_arr2, ARRAY_SIZE);	
 
 	cudaMemcpy(gpu_arr1, cpu_arr1, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
-	cudaMemcpy(gpu_arr2, gpu_arr2, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpu_arr2, cpu_arr2, ARRAY_SIZE_IN_BYTES, cudaMemcpyHostToDevice);
 
 	outputTemp1(gpu_arr1, gpu_arr2,ARRAY_SIZE);		
 
