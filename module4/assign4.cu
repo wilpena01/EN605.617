6632f3f2de10f55ae5a23ca3fb223441be31b52d
wilpena01 <thewilsonflow@hotmail.com>
2022-02-20 10:25:56 -0500

    organized

--- module4/assign4.cu
+++ module4/assign4.cu
@@ -140,7 +140,7 @@ int main(int argc, char** argv)
 
 	
 	start_time = get_time();	
-	main_Pinned(totalThreads, numBlocks, blockSize);
+	main_Pegeable(totalThreads, numBlocks, blockSize);
 	end_time = get_time();
 	cudaEventSynchronize(end_time);
 	cudaEventElapsedTime(&delta2, start_time, end_time);
