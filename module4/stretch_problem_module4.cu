2d5d93611d57004a2b53e0a561d5d25d0d84a4fb
wilpena01 <thewilsonflow@hotmail.com>
2022-02-20 21:31:57 -0500

    added strech problem

--- module4/stretch_problem_module4.cu
+++ module4/stretch_problem_module4.cu
@@ -1,4 +1,6 @@
-#include <stdio.h> #include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include "Utilities.h"
 
 /* 
  * The maximum and minimum integer values of the range of printable characters 
@@ -11,30 +13,44 @@
 
 …
 
-__global__ void encrypt(unsigned int *text, unsigned int *key, unsigned int *result) { /* Calculate the current index */ const unsigned int
-	idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+__global__ void encrypt(unsigned int *text, unsigned int *key, unsigned int *result) 
+{ /* Calculate the current index */ 
+
+	const unsigned int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 
 	 /* 
 	  * Adjust value of text and key to be based at 0 
 	  * Printable ASCII starts at MIN_PRINTABLE, but 0 start is easier to work with 
 	  */ 
-	char adjusted_text = text[idx] - MIN_PRINTABLE; 
-	char adjusted_key = key[idx] - MIN_PRINTABLE;
+	char adjusted_text = static_cast<char>(text[idx] - MIN_PRINTABLE); 
+	char adjusted_key = static_cast<char>(key[idx] - MIN_PRINTABLE);
 
 	 /* The cipher character is the text char added to the key char modulo the number of chars in the alphabet*/ 
 	char cipherchar = (adjusted_text + adjusted_key) % (NUM_ALPHA);
 
 	 /* adjust back to normal ascii (starting at MIN_PRINTABLE) and save to result */ 
-	result[idx] = (unsigned int) cipherchar + MIN_PRINTABLE ;
+	result[idx] = static_cast<unsigned int>(cipherchar + MIN_PRINTABLE);
 }
 
-void pageable_transfer_execution(int array_size, int threads_per_block, FILE *input_fp, FILE *key_fp) { /* Calculate the size of the array
-	*/ int array_size_in_bytes = (sizeof(unsigned int) * (array_size)); int i = 0;
+void pageable_transfer_execution(int array_size, int threads_per_block, FILE *input_fp, FILE *key_fp) 
+{ /* Calculate the size of the array*/ 
 
-	 unsigned int *cpu_text = (unsigned int *) malloc(array_size_in_bytes); 
-	 unsigned int *cpu_key = (unsigned int *) malloc(array_size_in_bytes); 
-	 unsigned int *cpu_result = (unsigned int *) malloc(array_size_in_bytes);
+	int array_size_in_bytes = (sizeof(unsigned int) * (array_size));
+	unsigned int *cpu_text = (unsigned int *) malloc(array_size_in_bytes); 
+	unsigned int *cpu_key = (unsigned int *) malloc(array_size_in_bytes); 
+	unsigned int *cpu_result = (unsigned int *) malloc(array_size_in_bytes);
 
+	// attempt to read the next line and store 
+	// the value in the "temp" variable 
+	unsigned int idx = 0;
+	char = temp;
+	while ( fscanf(input_fp, "%s", &temp ) == 1 && idx<array_size )  
+	{ 
+		cpu_text[idx] = static_cast<unsigned int>(temp);
+		idx++;
+	}
+
+	fscanf(key_fp,"%d", &cpu_key);
 	 /* Read characters from the input and key files into the text and key arrays respectively */ 
 	 // Code left out for brevity sake
 
@@ -67,67 +83,16 @@ void pageable_transfer_execution(int array_size, int threads_per_block, FILE *in
 	 print_encryption_results(cpu_text, cpu_key, cpu_result, array_size);
 
 	 /* Free the GPU memory */ 
-	 // INSERT CODE HERE
+	 cudaFree(gpu_text);
+	 cudaFree(gpu_key);
+	 cudaFree(gpu_result);
 
 	 /* Free the CPU memory */ 
-	 // INSERT CODE HERE
-
+	 free(cpu_text);
+	 free(cpu_key);
+	 free(cpu_result);
 }
 
-void pinned_transfer_execution(int array_size, int threads_per_block, FILE *input_fp, FILE *key_fp) { // Code left out for brevity sake
-
-	 //pin it cudaMallocHost((void **)&cpu_text_pinned, array_size_in_bytes); cudaMallocHost((void **)&cpu_key_pinned, array_size_in_bytes);
-	cudaMallocHost((void **)&cpu_result_pinned, array_size_in_bytes);
-
-	 /* Copy the memory over */ 
-	// INSERT CODE HERE
-
-	 /* Declare and allocate pointers for GPU based parameters */ 
-	unsigned int *gpu_text; 
-	unsigned int *gpu_key; 
-	unsigned int *gpu_result;
-
-	cudaMalloc((void **)&gpu_text, array_size_in_bytes); 
-	cudaMalloc((void **)&gpu_key, array_size_in_bytes); 
-	cudaMalloc((void **)&gpu_result, array_size_in_bytes);
-
-	/* Copy the CPU memory to the GPU memory */ 
-	cudaMemcpy( gpu_text, cpu_text_pinned, array_size_in_bytes, cudaMemcpyHostToDevice);
-	cudaMemcpy( gpu_key, cpu_key_pinned, array_size_in_bytes, cudaMemcpyHostToDevice);
-
-	 /* Designate the number of blocks and threads */ 
-	const unsigned int num_blocks = array_size/threads_per_block; 
-	const unsigned int num_threads = array_size/num_blocks;
-
-	 /* Execute the encryption kernel and keep track of start and end time for duration */ 
-	float duration = 0; 
-	cudaEvent_t start_time = get_time();
-
-	 encrypt<<<num_blocks, num_threads>>>(gpu_text, gpu_key, gpu_result);
-
-	 cudaEvent_t end_time = get_time(); 
-	 cudaEventSynchronize(end_time); 
-	 cudaEventElapsedTime(&duration, start_time, end_time);
-
-	 /* Copy the changed GPU memory back to the CPU */ 
-	 cudaMemcpy( cpu_result_pinned, gpu_result, array_size_in_bytes, cudaMemcpyDeviceToHost);
-
-	 printf("Pinned Transfer- Duration: %fmsn\n", duration); 
-	 print_encryption_results(cpu_text_pinned, cpu_key_pinned, cpu_result_pinned, array_size);
-
-	 /* Free the GPU memory */ 
-	cudaFree(gpu_text); 
-	cudaFree(gpu_key); 
-	cudaFree(gpu_result);
-
-	 /* Free the pinned CPU memory */ 
-	cudaFreeHost(cpu_text_pinned); 
-	cudaFreeHost(cpu_key_pinned); 
-	cudaFreeHost(cpu_result_pinned);
-
-	 /* Free the pageable CPU memory */ 
-	// INSERT CODE HERE 
-}
 
 /** * Prints the correct usage of this file * @name is the name of the executable (argv[0]) */ 
 void print_usage(char *name) {
@@ -137,64 +102,57 @@ void print_usage(char *name) {
 /** 
   * Performs simple setup functions before calling the pageable_transfer_execution() 
   * function. * Makes sure the files are valid, handles opening and closing of file pointers. 
-  */ void pageable_transfer(int num_threads, int threads_per_block, char *input_file, char *key_file) { 
+  */ void pageable_transfer(int num_threads, int threads_per_block) { 
 
-	// Code left out for brevity sake
+	FILE *input_fp, *key_fp;
+	input_fp = fopen("msg.txt","r");
+	key_fp = fopen("key.txt","r");
 
- 	/* Perform the pageable transfer */ 
-	pageable_transfer_execution(num_threads, threads_per_block, input_fp, key_fp);
-
+	if(input_fp != NULL && key_fp != NULL)
+	{
+		/* Perform the pageable transfer */ 
+		pageable_transfer_execution(num_threads, threads_per_block, input_fp, key_fp);           
+	}
+ 	
  	fclose(input_fp); fclose(key_fp); 
 }
 
-/** 
-  * Performs setup functions before calling the pageable_transfer_execution() 
-  * function. 
-  * Makes sure the files are valid, handles opening and closing of file pointers. 
-  */ void pinned_transfer(int num_threads, int threads_per_block, char *input_file, char *key_file) {
 
-	// Code left out for brevity sake
-
-    /* Perform the pageable transfer */ 
-    pinned_transfer_execution(num_threads, threads_per_block, input_fp, key_fp);
-
- 	fclose(input_fp); fclose(key_fp); 
-}
 
 	/** 
 	  * Entry point for excution. Checks command line arguments and 
 	  * opens input files, then passes execution to subordinate main_sub() 
 	  */
 int main(int argc, char *argv[]) { 
-	/* Check the number of arguments, print usage if wrong 
-	*/ 
-	if(argc != 5) { 
-		printf("Error: Incorrect number of command line arguments\n"); 
-		print_usage(argv[0]); exit(-1); 
+	
+	// read command line arguments
+	unsigned int totalThreads = 64;
+	unsigned int blockSize    = 32;
+	unsigned int numBlocks    = 8;
+	
+	if (argc >= 2) {
+        
+        sscanf(argv[1], "%d", &totalThreads);
+	}
+	if (argc >= 3) {
+        sscanf(argv[2], "%d", &blockSize);
 	}
 
- 	/* Check the values for num_threads and threads_per_block */ 
-	int num_threads = atoi(argv[1]); 
-	int threads_per_block = atoi(argv[2]);
-	if(num_threads <= 0 || threads_per_block <= 0) { 
-		printf("Error: num_threads and threads_per_block must be integer > 0");
-		print_usage(argv[0]); exit(-1); 
-	}
+	numBlocks = totalThreads/blockSize;
 
- 	if(threads_per_block > num_threads) { 
-		printf("Error: threads per block is greater than number of threads\n"); 
-		print_usage(argv[0]);
-		exit(-1); 
+	// validate command line arguments
+	if (totalThreads % blockSize != 0) 
+	{
+		++numBlocks;
+		totalThreads = numBlocks*blockSize;
+		
+		cout<<"Warning: Total thread count is not evenly divisible by the block size\n";
+		cout<<"The total number of threads will be rounded up to "<< totalThreads<<endl;
 	}
 
  	printf("\n"); 
 	/* Perform the pageable transfer */ 
-	pageable_transfer(num_threads, threads_per_block, argv[3], argv[4]);
-
- 	printf("-----------------------------------------------------------------\n");
-
- 	/* Perform the pinned transfer */ 
-	pinned_transfer(num_threads, threads_per_block, argv[3], argv[4]);
+	pageable_transfer(totalThreads, blockSize);
 
  	return EXIT_SUCCESS; 
 }
