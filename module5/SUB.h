d73af03e23be53c70755e6eafbe3c85e2f8ce85c
wilpena01 <thewilsonflow@hotmail.com>
2022-02-26 18:41:34 -0500

    fix bug

--- module5/SUB.h
+++ module5/SUB.h
@@ -2,9 +2,33 @@
 #define SUB_H
 
 #include "Utilities.h"
+#include <string>
+
+__constant__  static const UInt32 Input1 = 5;
+__constant__  static const UInt32 Input2 = 5;
 
 __global__
-void sub_arr(UInt32 *arr1, UInt32 *arr2, Int32 *Result,
+void add_Const(UInt32 *Block, UInt32 *Thread)
+{
+	const UInt32 thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	UInt32 result = (Input1 + thread_idx) + Input2;
+	Block[thread_idx]  = blockIdx.x;
+	Thread[thread_idx] = threadIdx.x;	
+}
+
+__global__
+void add_literal(UInt32 *Block, UInt32 *Thread)
+{
+	const UInt32 thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+
+	UInt32 result = (5 + thread_idx) + 5;
+	Block[thread_idx]  = blockIdx.x;
+	Thread[thread_idx] = threadIdx.x;	
+}
+
+__global__
+void add_arr(UInt32 *arr1, UInt32 *arr2, UInt32 *Result,
 			 UInt32 *Block, UInt32 *Thread)
 {
 	const UInt32 thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -31,7 +55,7 @@ void sub_arr_shared(UInt32 *arr1, UInt32 *arr2, Int32 *Result,
 	__syncthreads();
 }
 
-void runKernerSub(UInt32 *gpu_arr1, UInt32 *gpu_arr2, UInt32 num_blocks, 
+void runsharedMem(UInt32 *gpu_arr1, UInt32 *gpu_arr2, UInt32 num_blocks, 
                   UInt32 num_threads, Int32 *gpu_Result, UInt32 *gpu_Block,
 			      UInt32 *gpu_Thread)
 {
@@ -48,6 +72,26 @@ void runKernerSub(UInt32 *gpu_arr1, UInt32 *gpu_arr2, UInt32 num_blocks,
 	cudaEvent_t start2 = get_time();
 	sub_arr_shared<<<num_blocks, num_threads>>>(gpu_arr1, gpu_arr2, gpu_Result, 
 										 gpu_Block, gpu_Thread);
+	cudaEvent_t stop2 = get_time();	
+	cudaEventSynchronize(stop2);	
+	cudaEventElapsedTime(&delta2, start2, stop2);
+
+	string str[] ={"global", "shared"};
+	outputTime(delta1,delta2, str);
+}
+
+void runConstMem(UInt32 num_blocks, UInt32 num_threads, 
+				 UInt32 *gpu_Block, UInt32 *gpu_Thread)
+{
+	float delta1 = 0, delta2=0;
+	cudaEvent_t start1 = get_time();
+	add_literal<<<num_blocks, num_threads>>>(gpu_Block, gpu_Thread);
+	cudaEvent_t stop1 = get_time();	
+	cudaEventSynchronize(stop1);	
+	cudaEventElapsedTime(&delta1, start1, stop1);
+
+	cudaEvent_t start2 = get_time();
+	add_Const<<<num_blocks, num_threads>>>(gpu_Block, gpu_Thread);
 	cudaEvent_t stop2 = get_time();	
 	cudaEventSynchronize(stop2);	
 	cudaEventElapsedTime(&delta2, start2, stop2);
@@ -73,10 +117,12 @@ void Topsub(UInt32 *gpu_arr1, UInt32 *gpu_arr2,UInt32 num_blocks,
 	cudaMalloc((void **)&gpu_Result, ARRAY_SIZE_IN_BYTES1);
 	cudaMalloc((void **)&gpu_Block,  ARRAY_SIZE_IN_BYTES);
 	cudaMalloc((void **)&gpu_Thread, ARRAY_SIZE_IN_BYTES);
-cout<<"Addition Elapse Time:\n";
-	runKernerSub(gpu_arr1, gpu_arr2, num_blocks, num_threads, gpu_Result, 
+
+	cout<<"Addition Elapse Time:\n";
+	runsharedMem(gpu_arr1, gpu_arr2, num_blocks, num_threads, gpu_Result, 
 			     gpu_Block, gpu_Thread);
-cout<<"\n######################################\n";
+	runConstMem(num_blocks, num_threads, gpu_Block, gpu_Thread);
+	cout<<"\n######################################\n";
 	cudaMemcpy(cpu_Result, gpu_Result, ARRAY_SIZE_IN_BYTES1,cudaMemcpyDeviceToHost);
 	cudaMemcpy(cpu_Block,  gpu_Block,  ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
 	cudaMemcpy(cpu_Thread, gpu_Thread, ARRAY_SIZE_IN_BYTES, cudaMemcpyDeviceToHost);
