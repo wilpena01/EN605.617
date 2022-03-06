892e1c4313a729f41f0a1512c4a8f2557674c792
wilpena01 <thewilsonflow@hotmail.com>
2022-03-06 18:23:30 -0500

    added multiple stream

--- module7/assignment.cu
+++ module7/assignment.cu
@@ -39,7 +39,7 @@ RESULT run_add(UInt32 *gpu_arr1, UInt32 *gpu_arr2,
 	stop = get_time();	
 	cudaEventSynchronize(stop);	
 	cudaEventElapsedTime(&delta2, start, stop);
-	cout<<"Addition Execution Time\n";
+	cout<<"\nAddition Execution Time";
 	outputTime(delta1,delta2);
 	return addR;
 }
@@ -65,7 +65,7 @@ RESULT run_sub(UInt32 *gpu_arr1, UInt32 *gpu_arr2,
 	stop = get_time();	
 	cudaEventSynchronize(stop);	
 	cudaEventElapsedTime(&delta2, start, stop);
-	cout<<"Subtraction Execution Time\n";
+	cout<<"\nSubtraction Execution Time";
 	outputTime(delta1,delta2);
 
 	return subR;
@@ -93,7 +93,7 @@ RESULT run_mul(UInt32 *gpu_arr1, UInt32 *gpu_arr2,
 	stop = get_time();	
 	cudaEventSynchronize(stop);	
 	cudaEventElapsedTime(&delta2, start, stop);
-	cout<<"Multiplication Execution Time\n";
+	cout<<"\nMultiplication Execution Time";
 	outputTime(delta1,delta2);
 
 	return mulR;
@@ -120,7 +120,7 @@ RESULT run_mod(UInt32 *gpu_arr1, UInt32 *gpu_arr2,
 	stop = get_time();	
 	cudaEventSynchronize(stop);	
 	cudaEventElapsedTime(&delta2, start, stop);
-	cout<<"Modulo Execution Time";
+	cout<<"\nModulo Execution Time";
 	outputTime(delta1,delta2);
 
 	return modR;
