3cb846da548118283e1d7c05518b59806978ef41
wilpena01 <thewilsonflow@hotmail.com>
2022-03-27 23:00:09 -0400

    start

--- module8/Utilities.h
+++ module8/Utilities.h
@@ -98,8 +98,8 @@ void output(UInt32 *gpu_arr1, UInt32 *gpu_arr2, RESULT *outadd,
 }
 
 
-void outputTime(double duration1, 
-				double duration2, string *str)
+void outputTime(std::chrono::microseconds duration1, 
+				std::chrono::microseconds duration2, string *str)
 {
     /* print the duratino */
 	cout<<"\nElapsed Time Not using "<<str[0]<<" = "<< duration1<< " microsecond"
