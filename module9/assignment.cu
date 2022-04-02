a9d38cbe38edc577e19710937b402b6ecc2dea2a
wilpena01 <thewilsonflow@hotmail.com>
2022-04-01 21:51:42 -0400

    start

--- module9/assignment.cu
+++ module9/assignment.cu
@@ -122,26 +122,26 @@ int main()
     
     microseconds add_d1, add_d2;
     microseconds sub_d1, sub_d2;
-   /* microseconds mul_d1, mul_d2;
-    microseconds mod_d1, mod_d2;*/
+    microseconds mul_d1, mul_d2;
+    //microseconds mod_d1, mod_d2;
 
     thrust::host_vector<int> add(N);
     thrust::host_vector<int> sub(N);
-    /*thrust::host_vector<int> mul(N);
-    thrust::host_vector<int> mod(N); */
+    thrust::host_vector<int> mul(N);
+    //thrust::host_vector<int> mod(N); 
 
     addAnalysis(X,Y,add,add_d1,add_d2);
     subAnalysis(X,Y,sub,sub_d1,sub_d2);
-    /*mulAnalysis(X,Y,mul,mul_d1,mul_d2);
-    modAnalysis(X,Y,mod,mod_d1,mod_d2);*/
+    mulAnalysis(X,Y,mul,mul_d1,mul_d2);
+    //modAnalysis(X,Y,mod,mod_d1,mod_d2);
 
     string str[] ={"Thrus"};
     cout<<"X = ";   outputVec(X);
     cout<<"Y = ";   outputVec(Y);
     cout<<"add = "; outputVec(add); outputTime(add_d1,add_d2,str);
     cout<<"sub = "; outputVec(sub); outputTime(sub_d1,sub_d2,str);
-    /*cout<<"mul = "; outputVec(mul); outputTime(mul_d1,mul_d2,str);
-    cout<<"mod = "; outputVec(mod); outputTime(mod_d1,mod_d2,str);   */
+    cout<<"mul = "; outputVec(mul); outputTime(mul_d1,mul_d2,str);
+    //cout<<"mod = "; outputVec(mod); outputTime(mod_d1,mod_d2,str);   
 
     return 0;    
 }
