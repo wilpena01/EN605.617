55a951a7647d3044ea4720b31a582738c888e89e
wilpena01 <thewilsonflow@hotmail.com>
2022-04-17 16:53:31 -0400

    upload

--- module11/assignment.cpp
+++ module11/assignment.cpp
@@ -95,17 +95,14 @@ void createBuffer(cl_mem &inputSignalBuffer,
 int main(int argc, char** argv)
 {
     cl_int errNum;
-    cl_uint numPlatforms;
-	cl_uint numDevices;
+    cl_uint numPlatforms, numDevices;
     cl_platform_id * platformIDs;
 	cl_device_id * deviceIDs;
     cl_context context = NULL;
 	cl_command_queue queue;
 	cl_program program;
 	cl_kernel kernel;
-	cl_mem inputSignalBuffer;
-	cl_mem outputSignalBuffer;
-	cl_mem maskBuffer;
+	cl_mem inputSignalBuffer, outputSignalBuffer, maskBuffer;
 
     // First, select an OpenCL platform to run on.  
 	errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
