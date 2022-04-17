d230034f889aba1645c3fd67ca170c64a4958d8b
wilpena01 <thewilsonflow@hotmail.com>
2022-04-17 18:55:06 -0400

    upload

--- module11/Signal_mask.h
+++ module11/Signal_mask.h
@@ -261,7 +261,6 @@ cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
 }
 
 
-
 void createBuffer100(cl_mem &inputSignalBuffer,
 	cl_mem &outputSignalBuffer,
 	cl_mem &maskBuffer,  cl_context &context, cl_int &errNum)
@@ -292,7 +291,6 @@ void createBuffer100(cl_mem &inputSignalBuffer,
 	checkErr(errNum, "clCreateBuffer(outputSignal)");
 }
 
-
 void exeKernel100(cl_int &errNum, cl_kernel &kernel, cl_mem &inputSignalBuffer, 
 cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
 {
@@ -301,6 +299,216 @@ cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
     errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
 	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
 	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth100);
+	checkErr(errNum, "clSetKernelArg");
+
+	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
+    const size_t localWorkSize[2]  = { 1, 1 };
+
+    // Queue the kernel up for execution across the array
+    errNum = clEnqueueNDRangeKernel(
+		queue, 
+		kernel, 
+		2,
+		NULL,
+        globalWorkSize, 
+		localWorkSize,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueNDRangeKernel");
+    
+	errNum = clEnqueueReadBuffer(
+		queue, 
+		outputSignalBuffer, 
+		CL_TRUE,
+        0, 
+		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight, 
+		outputSignal,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueReadBuffer");
+}
+
+
+void createBuffer75(cl_mem &inputSignalBuffer,
+	cl_mem &outputSignalBuffer,
+	cl_mem &maskBuffer,  cl_context &context, cl_int &errNum)
+{
+    // Now allocate buffers
+	inputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
+		static_cast<void *>(inputSignal),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(inputSignal)");
+
+	maskBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * maskHeight75 * maskWidth75,
+		static_cast<void *>(mask75),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(mask)");
+
+	outputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_WRITE_ONLY,
+		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
+		NULL,
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(outputSignal)");
+}
+
+void exeKernel75(cl_int &errNum, cl_kernel &kernel, cl_mem &inputSignalBuffer, 
+cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
+{
+    errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
+	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth75);
+	checkErr(errNum, "clSetKernelArg");
+
+	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
+    const size_t localWorkSize[2]  = { 1, 1 };
+
+    // Queue the kernel up for execution across the array
+    errNum = clEnqueueNDRangeKernel(
+		queue, 
+		kernel, 
+		2,
+		NULL,
+        globalWorkSize, 
+		localWorkSize,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueNDRangeKernel");
+    
+	errNum = clEnqueueReadBuffer(
+		queue, 
+		outputSignalBuffer, 
+		CL_TRUE,
+        0, 
+		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight, 
+		outputSignal,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueReadBuffer");
+}
+
+
+void createBuffer50(cl_mem &inputSignalBuffer,
+	cl_mem &outputSignalBuffer,
+	cl_mem &maskBuffer,  cl_context &context, cl_int &errNum)
+{
+    // Now allocate buffers
+	inputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
+		static_cast<void *>(inputSignal),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(inputSignal)");
+
+	maskBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * maskHeight50 * maskWidth50,
+		static_cast<void *>(mask50),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(mask)");
+
+	outputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_WRITE_ONLY,
+		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
+		NULL,
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(outputSignal)");
+}
+
+void exeKernel50(cl_int &errNum, cl_kernel &kernel, cl_mem &inputSignalBuffer, 
+cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
+{
+    errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
+	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth50);
+	checkErr(errNum, "clSetKernelArg");
+
+	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
+    const size_t localWorkSize[2]  = { 1, 1 };
+
+    // Queue the kernel up for execution across the array
+    errNum = clEnqueueNDRangeKernel(
+		queue, 
+		kernel, 
+		2,
+		NULL,
+        globalWorkSize, 
+		localWorkSize,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueNDRangeKernel");
+    
+	errNum = clEnqueueReadBuffer(
+		queue, 
+		outputSignalBuffer, 
+		CL_TRUE,
+        0, 
+		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight, 
+		outputSignal,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueReadBuffer");
+}
+
+
+void createBuffer25(cl_mem &inputSignalBuffer,
+	cl_mem &outputSignalBuffer,
+	cl_mem &maskBuffer,  cl_context &context, cl_int &errNum)
+{
+    // Now allocate buffers
+	inputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
+		static_cast<void *>(inputSignal),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(inputSignal)");
+
+	maskBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * maskHeight25 * maskWidth25,
+		static_cast<void *>(mask25),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(mask)");
+
+	outputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_WRITE_ONLY,
+		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
+		NULL,
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(outputSignal)");
+}
+
+void exeKernel25(cl_int &errNum, cl_kernel &kernel, cl_mem &inputSignalBuffer, 
+cl_mem &outputSignalBuffer, cl_mem &maskBuffer, cl_command_queue &queue)
+{
+    errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
+	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth25);
 	checkErr(errNum, "clSetKernelArg");
 
 	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
