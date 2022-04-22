70ea13298401fbe397c9e7ce30a1dd70d3ef968f
wilpena01 <thewilsonflow@hotmail.com>
2022-04-21 22:19:43 -0400

    second part

--- module14/Final_Project/compression.cu
+++ module14/Final_Project/compression.cu
@@ -145,7 +145,7 @@ void compressionDriver_CL()
    {
       for(int j=0; j<height; j++)
       {
-         int idx = (i*height) + j;
+         int idx = (j*width) + i;
          //if(image2[idx]>256)
             cout<<"image ="<<image2[idx]<<"   ";
       }
