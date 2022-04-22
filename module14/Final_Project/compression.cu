57b742579a8a0545c951b080a048b4db76f5b10e
wilpena01 <thewilsonflow@hotmail.com>
2022-04-22 13:05:06 -0400

    second part

--- module14/Final_Project/compression.cu
+++ module14/Final_Project/compression.cu
@@ -137,7 +137,7 @@ void compressionDriver_CL()
                cudaMemcpy(cpu_Result, gpu_Result, HistSize_Byte, cudaMemcpyDeviceToHost);
                cudaMemcpy(cpu_Block,  gpu_Block,  HistSize_Byte, cudaMemcpyDeviceToHost);
                cudaMemcpy(cpu_Thread, gpu_Thread, HistSize_Byte, cudaMemcpyDeviceToHost);
-               //outputResult(cpu_Result, cpu_Block, cpu_Thread, 256);
+               outputResult(cpu_Result, cpu_Block, cpu_Thread, 256);
 
 
 
