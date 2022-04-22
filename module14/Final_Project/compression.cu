ab1ce18f5780b8b4bb14505d73aab2bda9671d33
wilpena01 <thewilsonflow@hotmail.com>
2022-04-22 15:23:00 -0400

    second part

--- module14/Final_Project/compression.cu
+++ module14/Final_Project/compression.cu
@@ -157,7 +157,8 @@ void compressionDriver_CL()
    //   cout<<"hist["<<i<<"] ="<<hist[i]<<"   ";
 
    //ocurrence_cu(hist, image, width, height)   ;
-   ocurrence_cu<<<image_num_blocks,image_num_threads>>>(g_image);
+   int uno=image_num_threads*image_num_threads;
+   ocurrence_cu<<<image_num_blocks,uno>>>(g_image);
    //ocurrence_cu<<<image_num_blocks,image_num_threads>>>(g_image);
    cudaDeviceSynchronize();
    //cudaMemcpy(g_hist,       hist,        HistSize_Byte,         cudaMemcpyHostToDevice);
