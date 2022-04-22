eb64ae367e61487738278c6bfa8624eb77d7a306
wilpena01 <thewilsonflow@hotmail.com>
2022-04-22 15:15:00 -0400

    second part

--- module14/Final_Project/Compress_Helper_cu.h
+++ module14/Final_Project/Compress_Helper_cu.h
@@ -216,7 +216,7 @@ void ocurrence_cu(int* image)
 
    //int i = image[thread_idx];
    //shared_hist[image[thread_idx]] += 1;
-   atomicAdd(shared_hist,1);
+   atomicAdd((shared_hist+254),1);
    //add_one_to_shared(i);
 
     __syncthreads();
