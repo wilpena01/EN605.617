5b0eca84fde0c2c7a40d3135c3435e1820767918
wilpena01 <thewilsonflow@hotmail.com>
2022-04-21 21:44:21 -0400

    fail

--- module14/Final_Project/Compress_Helper_cu.h
+++ module14/Final_Project/Compress_Helper_cu.h
@@ -101,9 +101,9 @@ void initHist_cu(int* hist, int *Result, int *Block, int *Thread)
 
    //copy_data_to_hist(0,idx);
    //copy_data_from_hist(hist,idx);
-   //shared_hist[idx] = 0;
-   //hist[idx] = 0;
-   Result[idx] = 5 ;
+   shared_hist[idx] = 0;
+   hist[idx] = 0;
+   Result[idx] = shared_hist[idx];
    Block[idx]  = blockIdx.x;
 	Thread[idx] = threadIdx.x;
   // __syncthreads();
