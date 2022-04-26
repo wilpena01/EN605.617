7daf3619ed1cde086aeb1e3ea945e7707dad3fea
wilpena01 <thewilsonflow@hotmail.com>
2022-04-25 21:18:23 -0400

    upload

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -251,7 +251,7 @@ int main(int argc, char** argv)
         checkErr(errNum, "clCreateKernel(average)");
 
         errNum = clSetKernelArg(kernel, 0, sizeof(cl_mem),    (void *)&buffers[i]); 
-        errNum = clSetKernelArg(kernel, 1, sizeof(int),    sum);
+        errNum = clSetKernelArg(kernel, 1, sizeof(cl_mem),    sum);
         //errNum = clSetKernelArg(kernel, 2, sizeof(float),    (void *)&sum[0]);
 
         checkErr(errNum, "clSetKernelArg(average)");
