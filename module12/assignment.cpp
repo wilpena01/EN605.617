fbca75ceb665fd4284c8a3964cad1f967ff27673
wilpena01 <thewilsonflow@hotmail.com>
2022-04-25 21:34:00 -0400

    upload

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -402,7 +402,7 @@ int main(int argc, char** argv)
 
         std::cout << std::endl;
     }
-    std::cout << "Average = " <<sum[0]<< std::endl;
+    //std::cout << "Average = " <<sum[0]<< std::endl;
     std::cout << "Elapse Time = " <<d.count()<< std::endl;
 
     return 0;
