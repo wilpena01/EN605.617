455c747afad9de25e785a57ee624e9156396eb28
wilpena01 <thewilsonflow@hotmail.com>
2022-04-25 20:25:52 -0400

    upload

--- module12/assignment.cpp
+++ module12/assignment.cpp
@@ -259,7 +259,7 @@ int main(int argc, char** argv)
 
         kernels.push_back(kernel);
     }
-        std::cout << "Average = " <<sum<< std::endl;
+        std::cout << "Average = " <<*sum<< std::endl;
 
 
     if (useMap) 
