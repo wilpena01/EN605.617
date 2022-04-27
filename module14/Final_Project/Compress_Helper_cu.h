72705cc1797d8e69e059ffe6522e0fc6454c66ba
wilpena01 <thewilsonflow@hotmail.com>
2022-04-27 19:49:17 -0400

    second part

--- module14/Final_Project/Compress_Helper_cu.h
+++ module14/Final_Project/Compress_Helper_cu.h
@@ -249,7 +249,7 @@ void minProp_cu(int* width, int* height, int *Result, int *Block, int *Thread)
       atomicMin(&shared_temp,ptemp);
    __syncthreads();
    shared_prob = shared_temp/100.0; 
-   Result[idx] = ptemp;
+   Result[idx] = *height;
    Block[idx]  = blockIdx.x+10;
 	Thread[idx] = threadIdx.x;
 
