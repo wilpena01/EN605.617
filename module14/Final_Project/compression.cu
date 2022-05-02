992da66e9fca55b666dccf0a47e8ed9be9b9cbc4
wilpena01 <thewilsonflow@hotmail.com>
2022-05-01 20:43:58 -0400

    second part

--- module14/Final_Project/compression.cu
+++ module14/Final_Project/compression.cu
@@ -260,7 +260,8 @@ void compressionDriver_CL()
 
 */
    BuildTree(pix_freq, huffcodes, *nodes);
-for(int ii =0; ii<300; ii++)
+   int ii;
+cin>>ii;
 cout<<ii<<endl;
    AssignCode(pix_freq, *nodes, *totalnodes);
    cout<<" aqui"<<endl;
