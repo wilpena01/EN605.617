6b1e6e28cf1a9f7facab582544d1949bca975f99
wilpena01 <thewilsonflow@hotmail.com>
2022-05-06 19:38:04 -0400

    second part

--- module14/Final_Project/compression.cu
+++ module14/Final_Project/compression.cu
@@ -139,7 +139,7 @@ int main()
    
    cout<<"Using Local CPU"<<endl;
    auto start = high_resolution_clock::now();
-   //compressionDriver();
+   compressionDriver();
    auto end = high_resolution_clock::now();
    auto duration = chrono::duration<double>(end-start);
 
